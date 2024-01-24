f4bd62540ff8892ec016a2337aa6331053ec53c5
Ashish Sonone <ashson07@gmail.com>
2024-01-24 22:53:50 +0530

    Add tests for ORDER_BY, GROUP_BY, LEFT_JOIN

--- test/mocha-test.js
+++ test/mocha-test.js
@@ -31,7 +31,7 @@ describe('SELECT', function () {
     });
 });
 
-describe.only('WHERE', function () {
+describe('WHERE', function () {
     it('WHERE using column letter', function () {
         const table = [['name', 'age'], ['Alice',2], ['Bob',4], ['Alice', 10]]
         const output = WHERE(table, "*A='Bob'")
@@ -82,4 +82,58 @@ describe.only('WHERE', function () {
     });
 });
 
-    
+describe('ORDER_BY', function () {
+    it('ORDER_BY single column DESC', function () {
+        const table = [['name', 'age'], ['Alice',21], ['Bob',4], ['Alice', 10]]
+        const output = ORDER_BY(table, "*age DESC")
+        assert.deepEqual(output, [['name', 'age'], ['Alice',21], ['Alice', 10], ['Bob',4]])
+    })
+
+    it('ORDER_BY single column refer by column letter', function () {
+        const table = [['name', 'age'], ['Alice',21], ['Bob',4], ['Alice', 10]]
+        const output = ORDER_BY(table, "*B ASC")
+        assert.deepEqual(output, [['name', 'age'], ['Bob',4], ['Alice', 10], ['Alice',21]])
+    })
+
+    it('ORDER_BY single column ASC', function () {
+        const table = [['name', 'age'], ['Alice',21], ['Bob',4], ['Alice', 10]]
+        const output = ORDER_BY(table, "*age ASC")
+        assert.deepEqual(output, [['name', 'age'], ['Bob',4], ['Alice', 10], ['Alice',21]])
+    })
+
+    it('ORDER_BY mulitiple columns numeric & text', function () {
+        const table = [['name', 'age'], ['Alice',21], ['Bob',40], ['Alice', 10]]
+        const output = ORDER_BY(table, "*name DESC, *age ASC")
+        assert.deepEqual(output, [['name', 'age'], ['Bob',40], ['Alice', 10], ['Alice',21]])
+    })
+})
+
+describe('GROUP_BY', function () {
+    it('GROUP_BY single column, sum+count agg', function () {
+        const table = [['name', 'age'], ['Alice',21], ['Bob',4], ['Alice', 10]]
+        const output = GROUP_BY(table, "*name", "$SUM(*age), $COUNT(1)")
+        assert.deepEqual(output, [['name', 'SUM B', 'COUNT 1'], ['Alice',31, 2], ['Bob', 4, 1]])
+    })
+
+    it('GROUP_BY multiple columns', function () {
+        const table = [['name', 'city', 'age'], ['Alice', 'Bombay', 21], ['Bob', 'Delhi', 4], ['Alice', 'Bombay', 4], ['Alice', 'Delhi', 10]]
+        const output = GROUP_BY(table, "*A,*B", "$COUNT(1)")
+        assert.deepEqual(output, [['name', 'city', 'COUNT 1'], ['Alice','Bombay', 2], ['Bob', 'Delhi', 1], ['Alice', 'Delhi', 1]])
+    })
+})
+
+describe('LEFT_JOIN', function () {
+    it('LEFT_JOIN single column', function () {
+        const students = [['name', 'age', 'school id'], ['Alice',21, 'S.1'], ['Bob',4, 'S.1'], ['Cathy', 10, 'S.2'], ['Darwin', 15, 'S.4']]
+        const schools = [['id', 'city'], ['S.1', 'BOM'], ['S.2','BLR'], ['S.3', 'DEL']]
+
+        const output = LEFT_JOIN(students, "*'school id'", schools, "*id")
+        assert.deepEqual(output, [
+            ['name', 'age', 'school id', 'id', 'city'],
+            ['Alice',21, 'S.1', 'S.1', 'BOM'],
+            ['Bob',4, 'S.1', 'S.1', 'BOM'],
+            ['Cathy', 10, 'S.2', 'S.2','BLR'],
+            ['Darwin', 15, 'S.4'] // no columns added here
+        ])
+    })
+})
